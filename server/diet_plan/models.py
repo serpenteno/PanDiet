01d2b3f3d9de34d9ba3150ab5ea873ce0c4c869a
serpenteno <fiacekk@gmail.com>
2024-11-11 21:57:38 +0100

    Meal model, DietPlan model, Tags enum

--- server/diet_plan/models.py
+++ server/diet_plan/models.py
@@ -3,27 +3,19 @@ from django.db import models
 from common.models import Tags
 
 
-class Meal(models.Model):
+class DietPlan(models.Model):
     VISIBILITY_CHOICES = [
-         ('public', 'Public'),
-         ('private', 'Private')
+        ("public", "Public"), 
+        ("private", "Private")
     ]
 
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True)
-    mass = models.DecimalField(max_digits=5, decimal_places=2)
     tags = models.IntegerField(choices=Tags.choices, default=0)
-    author = models.ForeignKey('users.User', on_delete=models.SET_NULL)
+    author = models.ForeignKey("users.User", on_delete=models.SET_NULL)
     visibility = models.CharField(max_length=10, choices=VISIBILITY_CHOICES)
 
-    def __str__(self):
-        return f"{self.name} added by {self.author} ({self.visibility})"
-
     def clean(self):
-        # Sprawdzamy, czy masa jest wiêksza od zera
-        if self.masss <= 0:
-            raise ValidationError("Mass must be a positive value")
-
         # Sprawdzamy, czy widocznoœæ jest poprawna
         if self.visibility not in dict(self.VISIBILITY_CHOICES):
             raise ValidationError("Invalid visibility")
